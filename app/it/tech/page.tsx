b5d843889ccf4d1318bf30c1a576845fd47e4720
Hirvilampi <lampinen.timo@gmail.com>
2026-01-14 15:21:37 +0200

    color change for dark mode in techs

--- app/it/tech/page.tsx
+++ app/it/tech/page.tsx
@@ -6,7 +6,7 @@ export default async function Technologies() {
   return (
     <>
       <div className="flex min-h-screen items-start justify-center bg-zinc-50 font-sans dark:bg-black">
-        <main className="flex w-full max-w-3xl flex-col items-center py-12 px-16 bg-white dark:bg-black sm:items-start ">
+        <main className="flex w-full max-w-3xl flex-col items-center py-12 px-16 bg-white dark:bg-black sm:items-start  text-black dark:text-zinc-50">
           <BackButton />
 
           <section>
